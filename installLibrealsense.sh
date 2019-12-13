f07ec47e0c426d402d846397868e20682876a8c0
Jetsonhacks <jim@jetsonhacks.com>
2019-12-13 15:05:25 -0800

    Update documentation link to librealsense master

--- installLibrealsense.sh
+++ installLibrealsense.sh
@@ -3,7 +3,7 @@
 # The installation is from a Intel RealSense Debian repository
 # Copyright (c) 2016-19 Jetsonhacks 
 # MIT License
-# https://github.com/dorodnic/librealsense/blob/jetson_doc/doc/installation_jetson.md
+# https://github.com/IntelRealSense/librealsense/blob/master/doc/installation_jetson.md
 
 # Register the server's public key:
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key F6E65AC044F831AC80A06380C8B3A55A6F3EFCDE || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key 
